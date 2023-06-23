1ed999124b81689f204fa4fe8d9be7f8eb3bc43c
Сергей <mak.sergei@ngs.ru>
2023-06-23 23:27:29 +0700

    One commit

---
+++ src/test/java/com/example/homeworkcalculator/HomeWorkCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
